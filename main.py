998c6db705c0b619f8ea1c5b19441f86a63fbc12
Peter Oliver Kolek <p.o.kolek@gmail.com>
2022-02-15 23:03:57 +0100

    Pridana zakladna BLEU metrika

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import keras
+import copy
 from mido import MidiFile, MidiTrack, MetaMessage, bpm2tempo, Message
 import pandas as pd
 import numpy as np
@@ -22,32 +23,41 @@ def open_midi(midi_file):
             if mid.tracks[i][j].channel == 9:
                 drum_track_number = i
                 break
-    return mid, mid.tracks[drum_track_number]
+    return mid, drum_track_number
 
 
 def get_transcription(drum_track, mid):
     # find ticks per beat, and divide it to Thirty-Second 32 notes
     ticks_per_beat_in_32_notes = mid.ticks_per_beat / 8
-    print(ticks_per_beat_in_32_notes)
+    print(mid.ticks_per_beat, ticks_per_beat_in_32_notes)
     # change notes time to stick it to 32 notes
     tmp_time = 0
-    time_with_note = {}
+    note_time_types = {}
     for i, message in enumerate(drum_track):
         # find time how it goes through song
         tmp_time += drum_track[i].time
+        print(drum_track[i].time, end=' ')
+        note_time_types[drum_track[i].time] = 1
         message.time = round(tmp_time / ticks_per_beat_in_32_notes)
         # make velocity of notes same
         if message.type == 'note_on':
             if message.velocity > 0:
                 message.velocity = 1
 
+        print(tmp_time, message.time, tmp_time / ticks_per_beat_in_32_notes, message.type)
+    print(note_time_types)
+
     # crating DataFrame for notes sticked to 32s and filter only note_on notes
     transcription = pd.DataFrame(m.dict() for m in drum_track)
+    print(transcription)
     transcription = transcription[transcription.type == 'note_on']
+    print(transcription)
     # modify table to have columns for every note and lines with time (32 notes as they folow the song)
     transcription = transcription.pivot_table(index='time', columns='note', values='velocity', fill_value=0)
+    print(transcription)
     # because we have 4/4 tempo, we have to add notes to have folowing 32 notes and empty values we fill with zeros
     transcription = transcription.reindex(pd.RangeIndex(transcription.index.max() + 1)).fillna(0).sort_index()
+    print(transcription)
     # retype to int
     transcription = transcription.astype(int)
     transcription.columns = transcription.columns.astype(int)
@@ -55,7 +65,7 @@ def get_transcription(drum_track, mid):
     return transcription
 
 
-def create_midi(tempo, transcription, ticks_per_beat):
+def create_midi(tempo, transcription, ticks_per_beat, file_name):
     # create new midi file
     new_mid = MidiFile()
     new_mid.ticks_per_beat = ticks_per_beat
@@ -101,74 +111,79 @@ def create_midi(tempo, transcription, ticks_per_beat):
                     drum_track_new.append(Message('note_on', channel=9, note=instruments[idx], velocity=80, time=0))
                     same_note_count += 1
             notes_from_last_message = 0
-    print(drum_track_new)
-    new_mid.save("./output/output.mid")
+    print(new_mid)
+    new_mid.save(file_name)
 
 
-mid, drum_track = open_midi('./RHCP_midi/FortuneFaded.mid')
+mid, drum_track_number = open_midi('./RHCP_midi/2ROCK.MID')
+drum_track = copy.deepcopy(mid.tracks[drum_track_number])
 
 transcription = get_transcription(drum_track, mid)
 print(transcription.values)
+transcription.to_csv("transcripion.csv")
 # find all instruments in song
 instruments = transcription.columns.tolist()
 
-inputs_list = []
-outputs_list = []
-sequence_len = 32
-raw_notes = transcription.values
-for i in range(len(raw_notes) - sequence_len):
-    input_start = i
-    input_end = i + sequence_len
-    output_start = input_end
-    output_end = output_start + 1
-
-    # for every 32 notes sequence set next note as output
-    inputs_list.append(raw_notes[input_start:input_end])
-    outputs_list.append(raw_notes[output_start:output_end])
-
-outputs_list = list(np.array(outputs_list).reshape(-1, np.array(outputs_list).shape[-1]))
-
-inputs_list = np.array(inputs_list)
-outputs_list = np.array(outputs_list)
-
-output_shape = outputs_list.shape[1]
-dropout = 0.3
-
-# very very very basic LSTM model
-model = Sequential()
-model.add(LSTM(sequence_len, input_shape=(sequence_len, len(instruments)), return_sequences=True, dropout=dropout))
-model.add(LSTM(sequence_len, return_sequences=True, dropout=dropout))
-model.add(LSTM(sequence_len, dropout=dropout))
-model.add(Dense(output_shape, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy',keras.metrics.BinaryCrossentropy()])
-
-mc = ModelCheckpoint(filepath='./new_encode_1st_try.h5', monitor='val_loss', verbose=1, save_best_only=True)
-
-history = model.fit(inputs_list, outputs_list, epochs=10, callbacks=mc, validation_split=0.1, verbose=1, shuffle=False)
-
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['accuracy', 'val_accuracy'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['loss', 'val_loss'], loc='upper left')
-
-plt.show()
-
-# predict new notes
-prediction = model.predict(inputs_list, verbose=0)
-# round prediction to 1 or 0
-prediction = np.around(prediction)
-# retype it to int
-prediction = prediction.astype(int)
-
-
-create_midi(120, prediction, mid.ticks_per_beat)
+bpm = 118
+create_midi(bpm, transcription.values, mid.ticks_per_beat, "./output/transcription_new.mid")
+#
+# inputs_list = []
+# outputs_list = []
+# sequence_len = 32
+# raw_notes = transcription.values
+# for i in range(len(raw_notes) - sequence_len):
+#     input_start = i
+#     input_end = i + sequence_len
+#     output_start = input_end
+#     output_end = output_start + 1
+#
+#     # for every 32 notes sequence set next note as output
+#     inputs_list.append(raw_notes[input_start:input_end])
+#     outputs_list.append(raw_notes[output_start:output_end])
+#
+# outputs_list = list(np.array(outputs_list).reshape(-1, np.array(outputs_list).shape[-1]))
+#
+# inputs_list = np.array(inputs_list)
+# outputs_list = np.array(outputs_list)
+#
+# output_shape = outputs_list.shape[1]
+# dropout = 0.3
+#
+# # very very very basic LSTM model
+# model = Sequential()
+# model.add(LSTM(sequence_len, input_shape=(sequence_len, len(instruments)), return_sequences=True, dropout=dropout))
+# model.add(LSTM(sequence_len, return_sequences=True, dropout=dropout))
+# model.add(LSTM(sequence_len, dropout=dropout))
+# model.add(Dense(output_shape, activation='sigmoid'))
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy',keras.metrics.BinaryCrossentropy()])
+#
+# mc = ModelCheckpoint(filepath='./new_encode_1st_try.h5', monitor='val_loss', verbose=1, save_best_only=True)
+#
+# history = model.fit(inputs_list, outputs_list, epochs=10, callbacks=mc, validation_split=0.1, verbose=1, shuffle=False)
+#
+# plt.plot(history.history['accuracy'])
+# plt.plot(history.history['val_accuracy'])
+# plt.title('Model accuracy')
+# plt.ylabel('Accuracy')
+# plt.xlabel('Epoch')
+# plt.legend(['accuracy', 'val_accuracy'], loc='upper left')
+# plt.show()
+#
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('Model loss')
+# plt.ylabel('Loss')
+# plt.xlabel('Epoch')
+# plt.legend(['loss', 'val_loss'], loc='upper left')
+#
+# plt.show()
+#
+# # predict new notes
+# prediction = model.predict(inputs_list, verbose=0)
+# # round prediction to 1 or 0
+# prediction = np.around(prediction)
+# # retype it to int
+# prediction = prediction.astype(int)
+#
+#
+# create_midi(120, prediction, mid.ticks_per_beat)
